10ddb6b890a87cbd226eea905f6146f73cac8055
Duccio Rocchini <ducciorocchini@gmail.com>
2022-10-14 17:25:06 +0200

    Update R_code_ecointeract.r

--- R_code_ecointeract.r
+++ R_code_ecointeract.r
@@ -2,7 +2,17 @@
 
 # We are using the sp package. To install it use:
 # install.packages("sp")
-library(sp)
+library(sp) # you can also make use of require()
+
+# We are using meuse:
+# search for: "meuse dataset R sp package"
+# https://cran.r-project.org/web/packages/gstat/vignettes/gstat.pdf
+
+data(meuse)
+
+View(meuse)
+
+
 
 
 
