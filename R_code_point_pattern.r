2c43123684e277c54bc84d4672607535add98e99
Duccio Rocchini <ducciorocchini@gmail.com>
2022-10-27 17:03:40 +0200

    Create R_code_point_pattern.r

---
+++ R_code_point_pattern.r
@@ -0,0 +1,33 @@
+# Point pattern analysis for population ecology
+
+# install.packages("spatstat")
+
+library(spatstat)
+
+# use of working directory
+setwd("~/lab/")
+
+covid <- read.table("covid_agg.csv", header=T)
+head(covid)
+
+attach(covid)
+covid_planar <- ppp(x=lon, y=lat, c(-180,180), c(-90,90))
+
+# WITHOUT ATTACHING:
+covid_planar <- ppp(x=covid$lon, y=covid$lat, c(-180,180), c(-90,90))
+
+density_map <- density(covid_planar)
+
+plot(density_map)
+points(covid_planar, pch=19)
+
+# changing colours
+
+cl <- colorRampPalette(c('yellow','orange','red'))(100) # 
+
+cl <- colorRampPalette(c("cyan", "coral", "chartreuse"))(100)
+plot(density_map, col=cl)
+points(covid_planar, pch=17, col="blue")
+
+# exercise
+# change the colours
