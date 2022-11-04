a7975bb38fbac9896574b9be6773543a56688030
Duccio Rocchini <ducciorocchini@gmail.com>
2022-11-04 17:06:17 +0100

    Update R_code_multivar.r

--- R_code_multivar.r
+++ R_code_multivar.r
@@ -18,6 +18,22 @@ plot(multivar)
 
 attach(biomes_types)
 
-ordiellipse(multivar, type, c("black", "red", "green", "blue"), kind="ehull", lwd=3)
-
 ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T)
+
+# let's save the graph out of R
+pdf("myfirstoutput.pdf")
+plot(multivar)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T)
+dev.off()
+
+# Exercise: export a pdf
+# with only the multivar plot
+# 5 minutes
+pdf("multivarperse.pdf")
+plot(multivar)
+dev.off()
+
+
+
