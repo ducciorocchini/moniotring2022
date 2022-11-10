f6b2de2681a9f0937c701a05a220970f9ede87d0
Duccio Rocchini <ducciorocchini@gmail.com>
2022-11-10 17:19:44 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -4,3 +4,13 @@
 install.packages("raster")
 
 library(raster)
+
+setwd("~/lab")
+
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
+
+plot(p224r63_2011)
+# dev.off()
+
+
+cl <- colorRampPalette(c('black','grey','light grey'))(100) # 
