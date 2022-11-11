2feaa354ea218b24fd428f969daec227c12c8555
Duccio Rocchini <ducciorocchini@gmail.com>
2022-11-11 17:37:17 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -1,7 +1,7 @@
 # I hate Duccio Rocchini
 # Me too!
 
-install.packages("raster")
+# install.packages("raster")
 
 library(raster)
 
@@ -14,3 +14,19 @@ plot(p224r63_2011)
 
 
 cl <- colorRampPalette(c('black','grey','light grey'))(100) # 
+plot(p224r63_2011, col=cl)
+
+# Plotting one band
+# solution 1
+plot(p224r63_2011$B1_sre, col=cl)
+
+plot(p224r63_2011[[1]], col=cl)
+
+# Exercise: change the colour ramp palette with colours from dark to light blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_2011[[1]], col=clb)
+
+par(mfrow=c(1,2))
+plot(p224r63_2011[[1]], col=cl)
+plot(p224r63_2011[[2]], col=cl)
+
