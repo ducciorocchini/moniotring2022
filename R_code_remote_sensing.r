0df8c3c814346be78611d1b2c06723639be7ee99
Duccio Rocchini <ducciorocchini@gmail.com>
2022-11-18 12:45:57 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -44,11 +44,53 @@ plot(p224r63_2011[[4]], col=cl)
 
 #Exercise: plot the four bands with different legends (colour ramps)
 par(mfrow=c(2,2))
+
 clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+plot(p224r63_2011[[1]], col=clb)
+
+clg <- colorRampPalette(c('dark green','green','light green'))(100) # 
+plot(p224r63_2011[[2]], col=clg)
+
+clr <- colorRampPalette(c('brown3','red','brown1'))(100) # 
+plot(p224r63_2011[[3]], col=clr)
+
+cln <- colorRampPalette(c('coral2','chartreuse3','yellow'))(100) # 
+plot(p224r63_2011[[4]], col=cln)
+
+#-----
+
+# multilayered colours
+
+dev.off()
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="lin")
+
+# plot the previous 4 manners in a single multiframe
+par(mfrow=c(2,2))
+plotRGB(p224r63_2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="lin")
+
+# dev.off()
+# histogram stretching
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="hist")
+
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
 
 
 
-colorRampPalette(c('dark blue','blue','light blue'))(100) # 
+
+
+
+
+
 
 
 
