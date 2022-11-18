81c704702353f7ec63a251f138bc032add02f678
Duccio Rocchini <ducciorocchini@gmail.com>
2022-11-18 17:18:48 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -84,9 +84,28 @@ par(mfrow=c(2,1))
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="lin")
 plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="hist")
 
+### Back in time
 
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+p224r63_2011 <- brick("p224r63_2011_masked.grd")
 
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="lin")
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="lin")
+plotRGB(p224r63_1988, r=3, g=4, b=2, stretch="lin")
 
+# Exercise: make a multiframe with 2 rows and 1 column
+# plotting the 1988 and the 2011 images
+# 5 minutes
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=3, g=2, b=4, stretch="lin")
+plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="lin")
+
+# calculating the difference between images
+# multitemporal analysis
+difnir <- p224r63_1988[[4]] - p224r63_2011[[4]]
+
+cl <- colorRampPalette(c('orange','yellow','black'))(100) # 
+plot(difnir, col=cl)
 
 
 
